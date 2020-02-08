6a5c3ea9ea13417fde96372c57299a523e3fea30
Jake Reske <33787429+reskejak@users.noreply.github.com>
2020-02-08 15:44:12 -0500

    Changed to iterations of 10,000 rows per df
    Original script was designed to append new rows to a growing df with each iteration. This became increasingly time consuming as iterations went on.
    
    Now each df is arranged by 10,000 iterations, then the 10,000 iteration dfs are appended.

--- GeneHancer_split.R
+++ GeneHancer_split.R
@@ -14,20 +14,37 @@
 
 # import GeneHancer (v4.4) database (n=218,177 enhancers)
 # download database via GeneCards: https://www.genecards.org/GeneHancer_version_4-4
-genehancer <- read.table("~/GeneHancer_version_4-4.txt", sep="\t", header=T)
+# import GeneHancer (v4.4) database
+genehancer <- read.table("/Users/jakereske/Google Drive/Chandler/public_datasets/GeneHancer_version_4-4.txt",
+                         sep="\t", header=T)
 
-# loop for splitting df, can take 1-2+ hours for all 218k enhancers.
+# prepare loop
+iters <- ceiling(nrow(genehancer)/10000) # round up
+last.iter <- nrow(genehancer) - ((iters-1)*10000)
+# loop in serial iterations of 10,000 rows
 genehancer.split <- data.frame(chrom=NA, source=NA, feature.name=NA, start=NA, end=NA, score=NA, strand=NA, frame=NA,
                                genehancer_id=NA, connected_gene=NA, connected_gene_score=NA)
-for (i in 1:nrow(genehancer)) {
-  atts <- strsplit(as.character(genehancer$attributes[i][[1]]), split=";")[[1]]
-  for (j in 1:(length(atts[-1])/2)) {
-    genehancer.split <- rbind(genehancer.split,
-                              cbind(genehancer[i,c(1:8)],
-                                    genehancer_id=strsplit(atts[1], split="=")[[1]][2], 
-                                    connected_gene=strsplit(atts[-1][(1+((j-1)*2))], split="=")[[1]][2],
-                                    connected_gene_score=strsplit(atts[-1][(2+((j-1)*2))], split="=")[[1]][2]))
+for (n in 1:iters) {
+  if (n < iters) {
+    iter.n <- (1:10000)+((n-1)*10000) 
+  } else {
+    iter.n <- (1:(nrow(genehancer) - ((iters-1)*10000)))+((n-1)*10000)
   }
+  genehancer.split.iter <- data.frame(chrom=NA, source=NA, feature.name=NA, start=NA, end=NA, score=NA, strand=NA, frame=NA,
+                                      genehancer_id=NA, connected_gene=NA, connected_gene_score=NA)
+  for (i in 1:nrow(genehancer[iter.n,])) {
+    print(paste(n,i,sep="_"))
+    atts <- strsplit(as.character(genehancer[iter.n,]$attributes[i][[1]]), split=";")[[1]]
+    for (j in 1:(length(atts[-1])/2)) {
+      genehancer.split.iter <- rbind(genehancer.split.iter,
+                                     cbind(genehancer[iter.n,][i,c(1:8)],
+                                           genehancer_id=strsplit(atts[1], split="=")[[1]][2], 
+                                           connected_gene=strsplit(atts[-1][(1+((j-1)*2))], split="=")[[1]][2],
+                                           connected_gene_score=strsplit(atts[-1][(2+((j-1)*2))], split="=")[[1]][2]))
+    }
+  }
+  genehancer.split <- rbind(genehancer.split,
+                            genehancer.split.iter)
 }
 genehancer.split <- genehancer.split[-1,] # remove initilization row
 # write.table(genehancer.split, file="GeneHancer_version_4-4_split.txt")
